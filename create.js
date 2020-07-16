d59188afede41c45d93532265968d52fbf28b8f8
Tom Bell <tjbell@gmail.com>
2020-07-16 11:54:35 -0600

    solution: admin and store

--- create.js
+++ create.js
@@ -3,14 +3,8 @@ AWS.config.region = 'us-west-2';
 AWS.config.apiVersion = '2012-11-05';
 
 const sqs = new AWS.SQS();
-
-const message = (err, data) => {
-  if (err) { console.log(`Error: ${err.message}`); }
-  else if (data) { console.log(`Success: ${JSON.stringify(data, null, 2)}`); }
-};
-
 const params = {
   QueueName: 'orders',
   Attributes: { 'ReceiveMessageWaitTimeSeconds': '10' }
 };
-sqs.createQueue(params, message);
+sqs.createQueue(params, messageJSON);
--- utils.js
+++ utils.js
@@ -3,4 +3,6 @@ const message = (err, data) => {
   else if (data) { console.log(`Success: ${JSON.stringify(data, null, 2)}`); }
 };
 
+const readJSON = (filename) => fs.existsSync(`json/${filename}.json`) ? JSON.parse(fs.readFileSync(`json/${filename}.json`)) : undefined;
+
 const randomID = () => '_' + Math.random().toString(36).substr(2, 9);
